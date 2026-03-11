7df0fbe0ab7ef85cd3db4c8d48fd7358b0085860
yogi2231 <yogiy7286@gmail.com>
2026-03-11 13:17:11 +0530

    secondcommitment

--- mainfolder/settings.py
+++ mainfolder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2+hykc+ilt30qrpws%m!mf&4i(l4iv6xtumlgns7rnnbbp1ore
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
